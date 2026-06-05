23148a73eb5f66aa6215cd41c27ed32f8e836bee
Sendyawldn <Sendyawaludin4568@gmail.com>
2026-06-05 09:59:43 +0700

    update

---
+++ jastipku-api/src/prisma/prisma-client.ts
@@ -0,0 +1,10 @@
+export {
+  Prisma,
+  PrismaClient,
+  OrderStatus,
+  TransactionType,
+  UserRole,
+  VerificationStatus,
+  WithdrawalStatus,
+} from ".prisma/client/default";
+} from "@prisma/client/default";
